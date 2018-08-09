62364a21006f47c5ceb1b05f9744fe35413afbce
serereuk <imssh186@gmail.com>
2018-08-09 09:55:24 +0900

    4주차 알고리즘 2번째 과제 수정

--- 2nd algorithm2.py
+++ 2nd algorithm2.py
@@ -1,17 +1,27 @@
 def repeat(temp):
-    if temp[1] != " ":
-        return ""
+    if len(temp) == 1:
+        return
     else:
         result = ""
-        num = int(temp[0])
         char = temp[2:]
         for o in char:
-            for i in range(num):
-                result = result + o
-
+            result = result + o * int(temp[0])
         return result
-
 
 temp = input("input: ") # 3 ABC 이런식으로 입력 따옴표 없이
 
 repeat(temp)
+
+def Main(temp):
+    result = ""
+    R = int(temp[0])
+    S = temp[2:]
+    for o in S:
+        result += o * R
+    return result
+nsim = input()
+for _ in range(int(nsim)):
+    temp = input()
+    print(Main(temp))
+
+# 아래꺼는 백준 통과용
